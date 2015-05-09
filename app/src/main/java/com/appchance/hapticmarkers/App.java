554f254c2b5600df49b310d4ee3d8813bad355b6
Jan Głuszczyński <jan.gluszczynski@gmail.com>
2015-05-09 15:36:08 +0200

    vibration test

--- app/src/main/java/com/appchance/hapticmarkers/App.java
+++ app/src/main/java/com/appchance/hapticmarkers/App.java
@@ -1,13 +1,27 @@
 package com.appchance.hapticmarkers;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.Vibrator;
 
 public class App extends Application {
+
+    private static Vibrator vibrator;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        
+    }
+
+    public static void vibrateOn() {
+        vibrator.vibrate(10 * 1000);
+    }
+
+    public static void vibrateOff() {
+        vibrator.cancel();
     }
 
 }
--- app/src/main/java/com/appchance/hapticmarkers/MainActivity.java
+++ app/src/main/java/com/appchance/hapticmarkers/MainActivity.java
@@ -4,13 +4,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    App.vibrateOn();
+                }
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    App.vibrateOff();
+                }
+
+                return false;
+            }
+        });
+
     }
 
     @Override
