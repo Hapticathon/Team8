1d8e4d1fef261cc6a36fffe03a5bfa7428869132
Jan Głuszczyński <jan.gluszczynski@gmail.com>
2015-05-09 15:17:02 +0200

    Initial commit

---
+++ app/src/main/java/com/appchance/hapticmarkers/App.java
@@ -0,0 +1,13 @@
+package com.appchance.hapticmarkers;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
